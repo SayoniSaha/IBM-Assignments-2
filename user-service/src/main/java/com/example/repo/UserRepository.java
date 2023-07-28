8829a45199f8786c1811d4a5aa20be18e62e71e0
Sayoni Saha <sahasayoni02@gmail.com>
2023-07-28 16:12:26 +0530

    Add files via upload

---
+++ user-service/src/main/java/com/example/repo/UserRepository.java
@@ -0,0 +1,12 @@
+package com.example.repo;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.example.model.UserEntity;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+	@Query
+	UserEntity findByUserId(String userId);
+
+}
